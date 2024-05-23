5fa540eb491e1597ae383df6eb567d7624a76537
graceallstaracademy <yihengy@graceallstaracademy.com>
2024-05-23 11:23:45 -0700

    correct the embed

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,17 +3,30 @@ import React from 'react';
 
 const ProjectList = () => {
   const projects = [
-    // Sample data
-    { id: 1, title: 'Project 1', url: 'https://scratch.mit.edu/projects/123456' },
-    { id: 2, title: 'Project 2', url: 'https://scratch.mit.edu/projects/789012' },
+    { id: 1017085571, title: '2024 new project', author: 'Katherine Wu' },
+    { id: 1023826293, title: 'Geometry Dash', author: 'Yitian Xu' },
+    { id: 1017097971, title: 'flying cat game', author: 'Ariana Bembenek' },
+    { id: 1017097965, title: 'cute cat', author: 'Zoey Bembenek' },
+    { id: 1017088134, title: 'None', author: 'Alexander Qi' },
+    { id: 1015707260, title: 'None', author: 'Caleb Liu' },
   ];
 
   return (
     <div>
       {projects.map((project) => (
-        <div key={project.id}>
+        <div key={project.id} className="project">
           <h2>{project.title}</h2>
-          <iframe src={project.url} width="485" height="402" allowtransparency="true" frameborder="0" scrolling="no" allowfullscreen></iframe>
+          <iframe
+            src={`https://scratch.mit.edu/projects/${project.id}/embed`}
+            allowTransparency="true"
+            width="485"
+            height="402"
+            frameBorder="0"
+            scrolling="no"
+            allowFullScreen
+            title={project.title}
+          ></iframe>
+          <h3>By {project.author}</h3>
         </div>
       ))}
     </div>
